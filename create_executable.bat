f8513427ef9478bf3755bf5c8f6a7277c6202645
ExuApplePie <admiral123456789@hotmail.com>
2023-04-29 20:47:13 -0400

    Fixed create_executable.bat

--- create_executable.bat
+++ create_executable.bat
@@ -1,9 +1,7 @@
-@echo off
-
 rem This script will create a venv and install the required packages and then create the executable
 rem This script will only work on Windows
 python -m venv venv
-venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
 pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --clean -F -n OptimalRecruitmentChecker main.py
